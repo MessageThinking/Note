2bf4d1d0abc318773958c81f6ff1af6372c6ff44
MessageThinking <wansandawang@gmail.com>
2017-10-17 23:38:11 +0800

    设计模式和相应修改

--- src/com/wansan/test/Test04.java
+++ src/com/wansan/test/Test04.java
@@ -6,15 +6,29 @@ package com.wansan.test;
  * 3.对成员变量进行初始化就是说会将其变成0或者null
  */
 public class Test04 extends Test02{
+	//这样写会直接打印出这个对象出来
+	private Test01 test0123=new Test01();
 	public String i;
-  static {
+	public String geti() {
+		this.i=i;
+		return i;
+	}
+  public Test01 getTest0123() {
+		return test0123;
+	}
+	public void setTest0123(Test01 test0123) {
+		this.test0123 = test0123;
+	}
+static {
 	  System.out.print("abds");
   }
   public Test04(){
 	  System.out.print("sssdd"+i);
+	  
   }
   public static void main(String[] args) {
 	  Test04 test=new Test04();
+	  System.out.println( test.geti()+test.getTest0123());
 	  //Test02 test02=new Test02();
      //  Test04 test04=(Test04) test02;
 

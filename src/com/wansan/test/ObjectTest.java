3256a53567dace3225ca81ef7be3c3fc982a5749
MessageThinking <wansandawang@gmail.com>
2017-11-05 15:02:45 +0800

    添加

---
+++ src/com/wansan/test/ObjectTest.java
@@ -0,0 +1,15 @@
+package com.wansan.test; 
+/** 
+* @author 作者：wanwei 
+* @version 创建时间：2017年11月4日 下午11:33:10 
+* 类说明 
+*/
+public class ObjectTest {
+public static void main(String[] args) {
+	Object object=new Object();
+	if(object==null) {
+		System.out.println("1111");
+	}
+	System.out.println(object);
+}
+}
